711b33b4655a200b569d4d469d43a4bbfeebd5a8
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-05-25 23:03:41 +0100

    Create a.cpp

---
+++ 2017/Q1/a.cpp
@@ -0,0 +1,26 @@
+// Score: 23/23 | 100%
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  string row;
+  cin >> row;
+  vector<char> a(row.begin(), row.end());
+  vector<char> b;
+  for(int _ = 0; _ < row.length() - 1; _++){
+    for(int i = 0; i < a.size() - 1; ++i){
+      if(a[i] == a[i+1]) {
+        b.push_back(a[i]);
+      } else {
+      if(a[i] != 'R' && a[i+1] != 'R') b.push_back('R');
+      else if(a[i] != 'G' && a[i+1] != 'G') b.push_back('G');
+      else if(a[i] != 'B' && a[i+1] != 'B') b.push_back('B');
+      }
+    }
+    a = b;
+    b.clear();
+  }
+  cout << a[0];
+}
