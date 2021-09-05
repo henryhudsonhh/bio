0d458125e4fb616b50f7098a08d42dca22cca106
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-09-05 17:46:27 +0100

    Create c.cpp

---
+++ 2012/Q2/c.cpp
@@ -0,0 +1,22 @@
+// Scores 6/6 | 100%
+
+/*
+
+For the explanation, I will use the notation that 1r = row 1 of the railway graph.
+
+The train starts at 3r -> 4r.
+
+The row order will be as follows:
+
+3r -> 4r
+4r -> 4r
+4r -> 3r
+3r -> 2r
+2r -> 1r
+1r -> 1r
+1r -> 2r
+2r -> 3r
+
+Because 1,000,000,000,000,000,000 can be divided by 8, and the length of this train journey is 8, we can say that the train will end up at 3r -> 4r, the row the train started from.
+
+*/
