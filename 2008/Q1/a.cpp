c6e19fd7d56f6ee9c0dfc6603d2e8d3b292677bb
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-11-11 14:30:22 +0000

    Update a.cpp

--- 2008/Q1/a.cpp
+++ 2008/Q1/a.cpp
@@ -1,1 +1,34 @@
+// Scores 25/25 | 100%
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+typedef vector<int> vi;
+
+#define pb push_back
+#define COUNT(v, i) count(v.begin(), v.end(), i)
+
+bool isPrime(int n) {
+  for (int i = 2; i < n; i++) if (n % i == 0) return false;
+  return true;
+}
+
+int main() {
+  int n, c = 0;
+  cin >> n;
+  vi check = {1};
+
+  for(int i = 2; i < n; i++) {
+    if(i % 2 == 0) continue;
+    else if(isPrime(i)){
+      if(isPrime(n - i) && !COUNT(check, n - i)) {
+        c++;
+        check.pb(i);
+      }
+    }
+  }
+  
+  cout << c;
+}
