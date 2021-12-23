9378429da7eb745ddb2638c0fc300716997685be
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-12-23 23:19:47 +0000

    Create d.py

---
+++ 2014/Q3/d.py
@@ -0,0 +1,12 @@
+# Scores 5/5 | 100%
+
+# One pair:
+# - ABUVWXYZ0123456789
+# - BCDEFGHIJKLMNOPQRS
+
+# Both must be 18 (36 / 2) characters long
+# Because if one password has one more character that the other, there must be 37 total characters used, which is more than 36.
+# First password must start with an A because all characters are used, and the passwords are ordered.
+# First password must be the final password starting with A of length 18 (must end with a 9 as well).
+# That last password is ABUVWXYZ0123456789 (first password)
+# The unused characteres, in order, are: BCDEFGHIJKLMNOPQRS (second password)
