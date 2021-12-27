f869385751a51d4f70368e78a29ab07130f8805b
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-12-27 21:36:39 +0000

    Update and rename a.cpp to a.py

---
+++ 2010/Q2/a.py
@@ -0,0 +1,97 @@
+# Scores 24/24 | 100%
+
+grid = [
+  ["x"] * 13,
+  ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"], 
+  ["x"] + [1] * 4 + [int(x) for x in input().split(" ")] + [1] * 4 + ["x"], 
+  ["x"] + [1] * 4 + [int(x) for x in input().split(" ")] + [1] * 4 + ["x"], 
+  ["x"] + [1] * 4 + [int(x) for x in input().split(" ")] + [1] * 4 + ["x"], 
+  ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"], ["x"] + [1] * 11 + ["x"],
+  ["x"] * 13,
+]
+
+def printGridFromPos(row, col):
+  print(f"{grid[row - 1][col - 1]}{grid[row - 1][col]}{grid[row - 1][col + 1]}\n{grid[row][col - 1]}{grid[row][col]}{grid[row][col + 1]}\n{grid[row + 1][col - 1]}{grid[row + 1][col]}{grid[row + 1][col + 1]}")
+
+def rotateDie():
+  if header == UP:
+    die[0], die[1], die[2], die[3], die[4], die[5] = die[3], die[0], die[1], die[2], die[4], die[5]
+  elif header == RIGHT:
+    die[0], die[1], die[2], die[3], die[4], die[5] = die[4], die[1], die[5], die[3], die[2], die[0]
+  elif header == DOWN:
+    die[0], die[1], die[2], die[3], die[4], die[5] = die[1], die[2], die[3], die[0], die[4], die[5]
+  elif header == LEFT:
+    die[0], die[1], die[2], die[3], die[4], die[5] = die[5], die[1], die[4], die[3], die[0], die[2]
+  else:
+    print("Invalid Header for Dice")
+
+def printGrid():
+  for i in range(len(grid)):
+    for j in range(len(grid[i])):
+      if i == row and j == col:
+        print(f"\033[4m{grid[i][j]}\033[0m", end=" ")
+      else:
+        print(grid[i][j], end=" ")
+    print()
+
+def moveDie():
+  global header, row, col
+  h = (die[0] + grid[row][col])
+  if h > 6:
+    h -= 6
+  grid[row][col] = h
+  
+  if h == 2:
+    header += 1
+    if header > 4:
+      header = 1
+  elif h == 5:
+    header -= 1
+    if header < 1:
+      header = 4
+  elif h != 1 and h != 6:
+    header += 2
+    if header > 4:
+      header -= 4
+  
+  d = dirc[header]
+
+  row += d[0]
+  col += d[1]
+
+  if grid[row][col] == "x":
+    if d[1] == 1:
+      col = 1
+    elif d[1] == -1:
+      col = 11
+    elif d[0] == 1:
+      row = 1
+    else:
+      row = 11
+  
+  rotateDie()
+
+n = int(input())
+
+UP, RIGHT, DOWN, LEFT = 1, 2, 3, 4
+
+dirc = {
+  UP: [-1, 0],
+  RIGHT: [0, 1],
+  DOWN: [1, 0],
+  LEFT: [0, -1]
+}
+
+header = UP
+
+row, col = 6, 6
+
+die = [1, 2, 6, 5, 3, 4]
+
+while n > 0:
+  for i in range(n):
+    moveDie()
+  
+  printGridFromPos(row, col)
+
+  n = int(input())
