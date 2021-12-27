77739003ee3e4fe084bdc713eee203e0ee41986d
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2021-12-27 21:55:14 +0000

    Create b.py

---
+++ 2010/Q2/b.py
@@ -0,0 +1,5 @@
+# Scores 2/2 | 100%
+
+# 32
+# The first cell must contain a 1, as the top of the dice contains a 1 to make 2, meaning the dice will go right.
+# There are 5 cells to the right, each can be either a 1 or a 6, so the answer is 2⁵.
