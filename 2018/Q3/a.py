eb2328417221e868314c958e0c5e119d396b01c3
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2023-01-07 00:40:53 +0000

    Update a.py

--- 2018/Q3/a.py
+++ 2018/Q3/a.py
@@ -45,3 +45,57 @@ constructGraph(1, root, root)
 t = max(smallest.values())
 
 print(t)
+
+# Second Solution, scores 24/24 | 100%
+
+n = int(input())
+num = input()
+
+def swapString(s, i1, i2):
+  s = list(s)
+  s[i1], s[i2] = s[i2], s[i1]
+  return "".join(s)
+
+graph = dict()
+
+def getNeighbours(s):
+  res = set()
+  for i in range(len(s) - 1):
+    if 0 < i < len(s) - 2:
+      [a, b, c, d] = [int(x) for x in s[i-1:i+3]]
+      if b < a < c or b < d < c or b > a > c or b > d > c:
+        res.add(swapString(s, i, i+1))
+    elif i == 0:
+      [a, b, c] = [int(x) for x in s[i:i+3]]
+      if a < c < b or a > c > b:
+        res.add(swapString(s, i, i+1))
+    else:
+      [a, b, c] = [int(x) for x in s[i-1:i+2]]
+      if c < a < b or c > a > b:
+        res.add(swapString(s, i, i+1))
+  return list(res)
+  
+foundNew = True
+seen = set()
+seen.add(num)
+graph[num] = 0
+depth = 0
+newlayer = [num]
+
+while foundNew:
+    depth += 1
+    layer = newlayer.copy()
+    newlayer = []
+
+    foundNew = False
+    while len(layer):
+      current = layer.pop(0)
+      neighbours = getNeighbours(current)
+      for node in neighbours:
+        if node not in seen:
+          newlayer.append(node)
+          seen.add(node)
+          foundNew = True
+          graph[node] = depth
+
+print(max(graph.values()))
