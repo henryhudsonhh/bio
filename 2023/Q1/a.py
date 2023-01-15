8017cfa20b7494cbe45294a08decc219c2916c32
henryhudsonhh <49619872+henryhudsonhh@users.noreply.github.com>
2023-01-15 14:54:25 +0000

    Update a.py

--- 2023/Q1/a.py
+++ 2023/Q1/a.py
@@ -1,1 +1,23 @@
 # Scores 24/24 | 100%
+
+def getLargestNumBelow(n):
+  fib1, fib2, fib3 = 0, 1, 1
+  if n <= 1:
+      return n
+  while (fib3 <= n):
+      fib1, fib2 = fib2, fib3
+      fib3 = fib1 + fib2
+  return fib2
+
+def solve(n):
+  nums = []
+
+  while n:
+    num = getLargestNumBelow(n)
+    nums.append(str(num))
+    n -= num
+    
+  res = " ".join(nums)
+  return res
+
+print(solve(int(input())))
